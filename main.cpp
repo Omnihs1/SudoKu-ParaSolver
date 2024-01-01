e42d3d0343d3461e64d08eb67af56b2626747cde
Omnihs1 <antandbee89@gmail.com>
2024-01-01 12:43:59 +0700

    add code

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char* argv[]) {
     // read sudoku board from file
     int board[boardSize * boardSize];
     ifstream myFile ("sudoku.txt");
-    for (int i = 0; i < boardSize * boardSize; i++) {
+    for (int i = 0; i < boardSize; i++) {
         for (int j = 0; j < boardSize; j++) 
             myFile >> board[i*boardSize + j];
     }
