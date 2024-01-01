0b26625ffbc839531bcecb1530eead02398b72a4
Omnihs1 <antandbee89@gmail.com>
2024-01-01 12:36:11 +0700

    add code

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <cmath>
+#include <vector>
+#include <cstring>
+#include <iostream>
+#include <fstream>
+#include <cuda_runtime.h>
+#include "parallelsudoku.cuh"
+
+#define UPDIV(n, d) (((n)+(d)-1) / (d))
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    // read sudoku board from file
+    int board[boardSize * boardSize];
+    ifstream myFiLE ("sudoku.txt");
+    for (int i = 0; i < boardSize * boardSize; i++) {
+        for (int j = 0; j < boardSize; j++) 
+            myFile >> board[i*boardSize + j];
+    }
+}
