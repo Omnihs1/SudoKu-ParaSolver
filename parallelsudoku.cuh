39a8562c22b0a734c585cb59b979ffff410efb24
Omnihs1 <antandbee89@gmail.com>
2024-01-02 21:37:46 +0700

    fix bug

--- parallelsudoku.cuh
+++ parallelsudoku.cuh
@@ -3,5 +3,5 @@
 const int boardSize = 9;
 // const int DEPTH = 5;
 void cudaSudokuSolver(int * old_board, int num, int*solution);
-void BoardGenerator(int *prev_board, int *pred_board_num, int* new_boards);
+void BoardGenerator(int *prev_board, int *pred_board_num, int* new_boards, int depth);
 #endif
